5ebee6a2dfc9537a467bef93954ec623950da0bc
KrrishSR4 <krishmishra4444@gmail.com>
2025-09-17 06:58:24 +0530

    operators

--- operators/NOT-operator.js
+++ operators/NOT-operator.js
@@ -5,4 +5,4 @@ let y = 6;
 console.log(! true) // false
 console.log(! false) // true
 console.log(!(x < y)) // false because x < y is true and not of true is false
-console.log(!(x > y)) // true because x > y is false and not of false is true
+console.log(!(x > y)) // true because x > y is false and not of false is true   
