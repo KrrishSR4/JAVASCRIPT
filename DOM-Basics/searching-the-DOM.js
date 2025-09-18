4eb30086bff4ceb3652d583e95005c42f4551fcf
KrrishSR4 <krishmishra4444@gmail.com>
2025-09-19 00:07:20 +0530

    dom complete

---
+++ DOM-Basics/searching-the-DOM.js
@@ -0,0 +1,12 @@
+// dom navigation properties are helpful when the elements are close to each other. If they are not clpse to each other, we have some methods to search the dom. 
+
+
+document.getElementById //this method is used to get the element with a given "id" attribute
+
+document.querySelectorAll //returns all elements inside an element matching the given css selector
+
+document.querySelector //returns the first element inside an element matching the given css selector
+
+document.getElementsByClassName //returns all elements inside an element with the given class name  
+
+document.getElementsByTagName //returns all elements inside an element with the given tag name
